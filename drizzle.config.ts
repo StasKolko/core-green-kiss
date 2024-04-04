045a0488371960a400334044265a862895c0ccaa
StasDev <b2bstas@gmail.com>
2024-04-04 19:47:16 +0300

    ci:add pull request check

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
+}
+
 export default {
   schema: "./db/schema.ts",
   out: "./db/migrations",
